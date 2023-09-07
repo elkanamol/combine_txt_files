5692d75126735bf86383035700971c3837385063
Elkana Molson <elkanam@telad-sys.local>
2023-09-07 17:31:11 +0300

    changing to cli tool
    changing to cli tool.
    adding flags to change different directory model file.
    adding debug prints.
    adding doc strings for functions.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # combine_txt_files
  cli tool to combine text files to one file
+
+
+## Log File Combiner
+
+This tool combines text log files for different IMEI numbers into a single output file.
+
+## Usage
+`python combine_imei_logs.py [-h] [-c CSV] [-m MODEL] [-s ADDRESS] [-u USERNAME] [-d]
+`
+#### Required arguments:
+
+`-c, --csv CSV` - Path to input CSV file containing list of IMEI numbers
+### Optional arguments:
+
+`-h, --help` - Show help message and exit.
+
+`-m, --model MODEL` - Model name (e.g. em9191) to determine log file location on server. Default is 'em9191'.
+
+`-s, --address ADDRESS` - IP address of SSH server. Default is '10.148.38.142'.
+
+`-u, --username USERNAME` - SSH username. Default is 'jade'.
+
+`-d, --debug` - Enable debug logging.
+
+## How it works
+- The script connects via SSH to a remote server containing log files for different IMEI numbers.
+- It takes a CSV file as input with the list of IMEI numbers to combine.
+- For each IMEI, it constructs the file path on the remote server and uses SSH to concatenate the contents.
+- A separator string is added between each log file in the output.
+- Finally, the combined log text is written to a local output file.
+
+Debug logging can be enabled to see details on the files being processed.
--- combaine_imei_log.py
+++ combaine_imei_log.py
@@ -1,11 +1,34 @@
 import os
 import csv
-from datetime import datetime
 import paramiko
+import argparse
+from datetime import datetime
 
-def combine_files(ssh, source_dir, output_file) -> None:
+# Define a global debug flag
+debug = False
+
+def debug_print(message) -> None:
+    if debug:
+        print(message)
+
+def combine_files(ssh, source_dir, input_file, output_file) -> None:
+    """Combines text log files for IMEI numbers into a single output file.
+
+        Parameters:
+        ssh (SSHClient): SSH connection to remote server
+        source_dir (str): Directory on server containing log files 
+        input_file (str): Local CSV file with list of IMEI numbers
+        output_file (str): Local path to write combined log file
+
+        This function:
+        - Opens the input CSV file containing IMEI numbers
+        - Constructs the log file path on the server for each IMEI
+        - Uses SSH to get the contents of each remote log file
+        - Appends the log text to the combined output
+        - Adds a separator string between each log file
+        - Writes the final combined text to the local output file
+    """
     combined_text = []
-    input_file = input("Enter input filename:  ") or 'imei_serials.csv'
 
     # Separator between log files
     separator = "==================================================================="
@@ -17,16 +40,19 @@ def combine_files(ssh, source_dir, output_file) -> None:
             imei = imei[0].strip()  # Assuming the serial number is in the first column
             directory_path = os.path.join(source_dir, imei)
             print(f"\r Reading imei: {imei} , Found!", end='\r', flush=True)
+            debug_print(f"Reading imei: {imei}, Found!")
 
             log_file_name = f"{imei}_log_file.txt"
             log_file_path = os.path.join(directory_path, log_file_name)
-            # print(f"logfile path {log_file_path}",end='\r', flush=True)
+            debug_print(f"log_file_path: {log_file_path}")
 
             try:
                 command = f"cat {log_file_path}"
+                debug_print(f"command: {command}")
                 stdin, stdout, stderr = ssh.exec_command(command)
-                outlines=stdout.readlines()
-                resp=''.join(outlines)
+                outlines = stdout.readlines()
+                resp = ''.join(outlines)
+                debug_print(resp)
                 combined_text.append(resp)
                 combined_text.append(separator)
             except FileNotFoundError:
@@ -39,23 +65,59 @@ def combine_files(ssh, source_dir, output_file) -> None:
     with open(output_file, 'w') as local_output_file:
         local_output_file.write(combined_text)
 
-if __name__ == "__main__":
-    date_file = datetime.utcnow().strftime("%Y-%m-%d")
-    source_directory = '/var/www/html/wiki/data/media/iot/em9191'
-    output_file = f'/home/jade/em91/logs/combined_log_{date_file}.txt'  # Update to your local output file path
+def main() -> None:
+    """Main function to handle command line arguments and orchestrate the log combining process.
+
+        This parses the command line arguments to get:
+        - Input CSV file path containing IMEI numbers 
+        - Model name to determine source log directory on server
+        - SSH server address and username
+
+        It then:
+        - Constructs full paths for source directory, output file
+        - Sets up SSH connection to remote server
+        - Calls combine_files() to concatenate the logs
+        - Closes the SSH connection when done
+
+        Any errors are printed, and a success message is printed when the process completes.
+        """
+    parser = argparse.ArgumentParser(description="Combine IMEI log files")
+    parser.add_argument("-c", "--csv", type=str, help="Input CSV file containing IMEI serial numbers, or path for file if it not in same directoy")
+    parser.add_argument("-m", "--model", type=str, help="Model name (e.g., em9191) to specify source directory")
+    parser.add_argument("-s", "--address", type=str, help="SSH server IP address, the default is 10.148.38.142")
+    parser.add_argument("-u", "--username", type=str, help="remote user name to connect with SSH")
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+
+
+
+    args = parser.parse_args()
+
+    global debug
+    input_file = args.csv or 'imei_serials.csv'
+    model = args.model or 'em9191'
+    username = args.username or 'jade'
+    server_address = args.address or '10.148.38.142'
+    debug = args.debug
     
 
-    print("Start reading the log files")
+
+    source_directory = f'/var/www/html/wiki/data/media/iot/{model}'
+    date_file = datetime.utcnow().strftime("%Y-%m-%d")
+    output_file = f'/home/jade/em91/logs/combined_log_{date_file}.txt'
+
     # SSH connection to the remote WIKI server
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    debug_print(f"conneting to ssh {username}@{server_address}")
 
     try:
-        ssh.connect('10.148.38.142', username='jade')
-        # Call the combine_files function with the SSH connection
-        combine_files(ssh, source_directory, output_file)
+        ssh.connect(server_address , username=username)
+        combine_files(ssh, source_directory, input_file , output_file)
     except Exception as e:
         print(f"Error: {str(e)}")
     finally:
-        print("Process completed sucessfuly") 
+        print("Process completed successfully")
         ssh.close()
+
+if __name__ == "__main__":
+    main()
